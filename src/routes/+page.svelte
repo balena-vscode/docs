e23b7b63f021954c0c31a8ba1d60a5afbe6d857a
Aiden Schrock <aidennoelschrock@gmail.com>
2022-12-19 19:07:01 -0600

    design change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,109 +1,27 @@
-<div class="title">
-	<h1>Manage your IoT devices without leaving your workspace</h1>
-	<h2>Unoffical VS Code extenstion for integration with Balena</h2>
-	<div class="buttons">
-		<a
-			rel="noreferrer"
-			target="_blank"
-			href="https://marketplace.visualstudio.com/items?itemName=kalebpace.balena-vscode"
-		>
-			<button class="vs-button" type="button">VS Marketplace</button>
-		</a>
-		<a
-			rel="noreferrer"
-			target="_blank"
-			href="https://open-vsx.org/extension/kalebpace/balena-vscode"
-		>
-			<button class="vsx-button" type="button">Open VSX</button>
-		</a>
-	</div>
-	<picture>
-		<source srcset="balena-asset-simple.png" type="png" />
-		<img
-			class="balena-asset"
-			alt="extension visualization in VS code"
-			src="balena-asset-simple.png"
-			width="800px"
-			height="600px"
-		/>
-	</picture>
-</div>
-<div class="cards">
-	<div class="card">
-		<picture>
-			<source srcset="summary.svg" type="svg/xml" />
-			<img width="60px" height="45px" alt="list icon" src="summary.svg" />
-		</picture>
-		<div>
-			<h3 class="card_title">Summary</h3>
-			<p class="card_text">
-				View a rich, interactive summary. Common information and state is easily copied, toggled, or
-				updated
-			</p>
+<div class="header">
+	<div class="title">
+		<h1>Manage your IoT devices without leaving your workspace</h1>
+		<h2>Unoffical VS Code extenstion for integration with Balena</h2>
+		<div class="buttons">
+			<a
+				rel="noreferrer"
+				target="_blank"
+				href="https://marketplace.visualstudio.com/items?itemName=kalebpace.balena-vscode"
+			>
+				<button class="vs-button" type="button">VS Marketplace</button>
+			</a>
+			<a
+				rel="noreferrer"
+				target="_blank"
+				href="https://open-vsx.org/extension/kalebpace/balena-vscode"
+			>
+				<button class="vsx-button" type="button">Open VSX</button>
+			</a>
 		</div>
 	</div>
-	<div class="card">
-		<picture>
-			<source srcset="devices.svg" type="svg/xml" />
-			<img width="60px" height="45px" alt="list icon" src="devices.svg" />
-		</picture>
-		<div>
-			<h3 class="card_title">Devices</h3>
-			<p class="card_text">Easily view and filter your devices for the active fleet</p>
-		</div>
+	<div>
+		<h1>Image Placeholder</h1>
 	</div>
-	<div class="card">
-		<picture>
-			<source srcset="verified.svg" type="svg/xml" />
-			<img width="60px" height="60px" alt="list icon" src="verified.svg" />
-		</picture>
-		<div>
-			<h3 class="card_title">Releases</h3>
-			<p class="card_text">View and filter your releases, see build status, and list services</p>
-		</div>
-	</div>
-	<div class="card">
-		<picture>
-			<source srcset="wrench.svg" type="svg/xml" />
-			<img width="60px" height="65px" alt="list icon" src="wrench.svg" />
-		</picture>
-		<div>
-			<h3 class="card_title">Services</h3>
-			<p class="card_text">View service status, logs, and control status</p>
-		</div>
-	</div>
-	<div class="card">
-		<picture>
-			<source srcset="variable.svg" type="svg/xml" />
-			<img width="60px" height="45px" alt="list icon" src="variable.svg" />
-		</picture>
-		<div>
-			<h3 class="card_title">Variables</h3>
-			<p class="card_text">View and edit your fleet wide or device variables</p>
-		</div>
-	</div>
-	<div class="card">
-		<picture>
-			<source srcset="metadata.svg" type="svg/xml" />
-			<img width="60px" height="60px" alt="list icon" src="metadata.svg" />
-		</picture>
-		<div>
-			<h3 class="card_title">Metadata</h3>
-			<p class="card_text">
-				Can’t find the information you need in any view? View the entirety of an SDK model’s meta!
-			</p>
-		</div>
-	</div>
-</div>
-<div class="contribute">
-	<h2>
-		Want to contribute? Check out <a
-			rel="noreferrer"
-			target="_blank"
-			href="https://github.com/balena-vscode/balena-vscode/blob/main/CONTRIBUTING.md"
-			>CONTRIBUTING.md</a
-		> in the repo
-	</h2>
 </div>
 
 <style>
@@ -113,30 +31,16 @@
 	h2 {
 		margin-top: 0;
 		font-size: 16px;
-		color: #222832;
+		color: #d9d9d9;
 	}
 
-	a {
-		color: #114783;
-		text-decoration: none;
-	}
-
-	a:hover {
-		color: #2176d4;
-	}
-
-	p {
-		color: #222832;
-	}
 	.title {
-		width: 90%;
-		margin-top: 30px;
+		max-width: 400px;
 	}
 
-	.balena-asset {
-		width: 90vw;
-		max-width: 1440px;
-		height: auto;
+	.header {
+		flex-direction: column;
+		height: 100%;
 	}
 
 	button {
@@ -169,57 +73,12 @@
 		color: #222832;
 	}
 
-	.cards {
-		display: flex;
-		flex-direction: row;
-		flex-wrap: wrap;
-		margin: 0;
-		margin-top: 58px;
-	}
-
-	.card {
-		display: flex;
-		flex-direction: row;
-		margin-top: 8px;
-		height: 120px;
-		width: 100%;
-		justify-content: unset;
-	}
-
-	.card div {
-		display: block;
-		text-align: left;
-		margin: 0;
-	}
-
-	.card h3 {
-		font-size: 18px;
-		margin-bottom: 0px;
-		margin-top: 8px;
-	}
-
-	.card p {
-		font-size: 16px;
-		margin-top: 8px;
-	}
-
-	.card img {
-		width: 40px;
-		margin-right: 16px;
-		height: auto;
-	}
-
-	.contribute {
-		margin-top: 150px;
-		margin-bottom: 100px;
-	}
-
 	@media (min-width: 768px) {
 		h1 {
 			font-size: 40px;
 		}
 		h2 {
-			font-size: 24px;
+			font-size: 22px;
 		}
 
 		button {
@@ -228,44 +87,17 @@
 			font-size: 24px;
 			margin: 16px;
 		}
-
-		.card {
-			border: #ffc100 solid 2px;
-			border-radius: 10px;
-			display: flex;
-			flex-direction: row;
-			padding: 8px 16px;
-			width: 43%;
-			margin-top: 16px;
-			height: 140px;
-		}
-
-		.card div {
-			width: 80%;
-		}
-
-		.card h3 {
-			font-size: 26px;
-		}
-
-		.card p {
-			font-size: 18px;
-		}
-
-		.card img {
-			width: 60px;
+		.title {
+			max-width: 600px;
 		}
 	}
 	@media (min-width: 1024px) {
 		.title {
 			width: 80%;
 		}
-
-		.balena-asset {
-			width: 60vw;
-		}
-		.card {
-			height: 120px;
+		.header {
+			flex-direction: row;
+			/* width: 85%; */
 		}
 	}
 
---
+++ src/routes/docs/+page.svelte
@@ -0,0 +1,9 @@
+<div class="header">
+	<h1>Coming Soon</h1>
+</div>
+
+<style>
+	.header {
+		height: 100%;
+	}
+</style>
